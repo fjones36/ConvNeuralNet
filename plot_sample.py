2c8d68fbe70a0bfa1b6c8232590fb45c1df86d8d
Frank J <fjones36@gmail.com>
2015-03-07 09:46:45 -0800

    Added Plotting function to plot_sample

---
+++ plot_sample.py
@@ -0,0 +1,17 @@
+from scipy import *
+import matplotlib.pyplot as plt
+
+def plot_sample(x, y, axis):
+	img = x.reshape(96, 96)
+	axis.imshow(img, cmap='gray')
+	axis.scatter(y[0::2]*48 + 48, y[1::2]*48 + 48, marker='x', s=10)
+
+def create_plot(X, y_pred):
+	fig = plt.figure(figsize=(6,6))
+	fig.subplots_adjust(
+		left=0, right=1, botom=0, top=1, hspace=0.05, wspace=0.05)
+
+	for i in range(16):
+		ax = fig.add_subplot(4, 4, i+1, xticks=[], yticks=[])
+		plot_sample(X[i], y_pred[i], ax)
+	plt.show()
